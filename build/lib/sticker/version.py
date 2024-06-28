4c37a534363542ca1fdd07de8ad6595498a69ce2
Virkkunen <vrkknn@vrkknn.net>
2024-06-28 23:10:13 +0200

    stickers

--- build/lib/sticker/version.py
+++ build/lib/sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = 'f293e064'
-version = '0.1.0+dev.f293e064'
-linkified_version = '0.1.0+dev.[f293e064](https://github.com/maunium/stickerpicker/commit/f293e064a7be551612f38687a1b21c6a29e8e91d)'
+git_revision = 'b401b5e9'
+version = '0.1.0+dev.b401b5e9'
+linkified_version = '0.1.0+dev.[b401b5e9](https://github.com/maunium/stickerpicker/commit/b401b5e98e0b0e3a116511e3dcb0cbc1e2da5617)'
