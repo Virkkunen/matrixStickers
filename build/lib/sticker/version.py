5ce14bd2c587e71fa0244a8e341ef8d4885a4ec8
Virkkunen <vrkknn@vrkknn.net>
2024-12-17 17:47:30 +0100

    commit

--- build/lib/sticker/version.py
+++ build/lib/sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = 'b401b5e9'
-version = '0.1.0+dev.b401b5e9'
-linkified_version = '0.1.0+dev.[b401b5e9](https://github.com/maunium/stickerpicker/commit/b401b5e98e0b0e3a116511e3dcb0cbc1e2da5617)'
+git_revision = '591c7012'
+version = '0.1.0+dev.591c7012'
+linkified_version = '0.1.0+dev.[591c7012](https://github.com/maunium/stickerpicker/commit/591c70127dc2d11f6356a2687966855bf835e3cc)'
