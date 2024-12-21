fb5da9473ef24d37e1e0c6a964bc5a7221795583
Virkkunen <vrkknn@vrkknn.net>
2024-12-21 20:07:08 +0100

    packs

--- build/lib/sticker/version.py
+++
@@ -1,6 +0,0 @@
-# Generated in setup.py
-
-git_tag = None
-git_revision = '591c7012'
-version = '0.1.0+dev.591c7012'
-linkified_version = '0.1.0+dev.[591c7012](https://github.com/maunium/stickerpicker/commit/591c70127dc2d11f6356a2687966855bf835e3cc)'
